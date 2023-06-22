d3304eba5f04f3367e988881d56b9646d25f954a
Brian Adams <brianadamsmaino68@gmail.com>
2023-06-22 19:46:34 +0200

    Lab in progress

--- js/game.js
+++ js/game.js
@@ -1,3 +1,67 @@
 class Game {
-    // code to be added
+    // code to be added 
+    constructor() {
+
+        this.startScreen = document.getElementById
+        ('game-intro')
+        this.gameScreen = document.getElementById
+        ('game-screen')
+        this.gameEndScreen = document.getElementById
+        ('game-end')
+
+        this.player = null;
+        this.height = 600
+        this.width = 500
+        this.player = new Player(this.gameScreen)
+        this.obstacle = [new Obstacle(this.gameScreen)]
+        this.isGameOver = false
+        this.score = 0
+        this.lives = 3
+    }
+
+    start() {
+        this.gameScreen.style.width = `${this.width}px`
+        this.gameScreen.style.height = `${this.height}px`
+
+        this.startScreen.style.display = 'none'
+        this.gameScreen.style.display = 'block'
+
+        this.gameLoop()
+    }
+
+    gameLoop() {
+        this.update()
+
+        if (Math.random() > 0.98 && this.obstacle.length < 1) {
+            this.obstacle.push(new Obstacle(this.gameScreen));
+          }
+
+
+          if(this.isGameOver){
+            console.log('GameOver')
+          }
+        requestAnimationFrame(() => this.gameLoop())
+    }
+
+    update() {
+        this.player.move()
+        const obstacleToKeep = []
+        this.obstacle.forEach(obstacle => {
+            obstacle.move()
+            if (this.player.didCollide(obstacle)) {
+                obstacle.element.remove();
+                this.lives -= 1
+            } else if (obstacle.top > this.gameScreen.offsetHeight) {
+                this.score += 1
+            } else {
+                obstacleToKeep.push(obstacle)
+            }
+        })
+
+        this.obstacle = obstacleToKeep;
+
+        if(this.lives <= 0){
+            this.isGameOver = true 
+        }
+    }
 }
---
+++ js/player.js
@@ -0,0 +1,66 @@
+class Player {
+
+    constructor(gameScreen) {
+         this.gameScreen = gameScreen
+         this.with = 80
+         this.height = 150
+         this.top = 480
+         this.left = 210
+         this.directionX = 0
+         this.directionY = 0
+         this.element = document.createElement('img')
+
+         this.element.src = './images/car.png'
+         this.element.style.position = 'absolute'
+
+         this.element.style.width = `${this.width}px`
+         this.element.style.height = `${this.height}px`
+
+         this.element.style.top = `${this.top}px`
+         this.element.style.left = `${this.left}px`
+
+         this.gameScreen.appendChild(this.element)
+    }
+
+    move() {
+        this.left += this.directionX;
+        this.top += this.directionY;
+        if(this.left < 10) {
+            this.left = 10
+        }
+        if(this.top < 0) {
+            this.top = 0
+        }
+        // handles right hand side
+        if (this.left > this.gameScreen.offsetWidth - this.with - 50) {
+            this.left = this.gameScreen.offsetWidth - this.with - 50
+        }
+        // handles bottom side
+        if (this.top > this.gameScreen.offsetHeight - this.height - 10) {
+            this.top = this.gameScreen.offsetHeight - this.height - 10;
+        }
+
+        this.updatePosition();
+    }
+
+    updatePosition() {
+        this.element.style.top = `${this.top}px` 
+        this.element.style.left = `${this.left}px`
+     }
+
+     didCollide(obstacle) {
+        const playerRect = this.element.getBoundingClientRect();
+        const obstacleRect = obstacle.element.getBoundingClientRect();
+    
+        if (
+          playerRect.left < obstacleRect.right &&
+          playerRect.right > obstacleRect.left &&
+          playerRect.top < obstacleRect.bottom &&
+          playerRect.bottom > obstacleRect.top
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+}
